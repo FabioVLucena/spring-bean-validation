43ee28ec9e34ba8e7f53a377439390c9b765d575
aeon <yasunofabio@outlook.com.br>
2023-05-04 23:38:58 -0400

    adiciona PessoaResponseDTO

---
+++ src/main/java/com/aeon/validation/dto/PessoaResponseDTO.java
@@ -0,0 +1,16 @@
+package com.aeon.validation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PessoaResponseDTO {
+
+	private String nomeRazao;
+
+	private String pessoaTipo;
+	
+	private String estadoCivil;
+	
+}
