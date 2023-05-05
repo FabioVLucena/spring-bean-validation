4c3b9c1571f00e4f7eff07c58656d30bae88789f
aeon <yasunofabio@outlook.com.br>
2023-05-05 00:00:41 -0400

    adiciona validacoes aos campos nomeRazao e cpfCnpj do requestDTO

--- src/main/java/com/aeon/validation/dto/PessoaRequestDTO.java
+++ src/main/java/com/aeon/validation/dto/PessoaRequestDTO.java
@@ -1,5 +1,10 @@
 package com.aeon.validation.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,8 +12,12 @@ import lombok.Setter;
 @Setter
 public class PessoaRequestDTO {
 
+	@NotEmpty(message = "Nome Razão não pode ser vazio.")
+	@NotNull(message = "Nome Razão é um campo obrigatório.")
 	private String nomeRazao;
 
+	@CPF(message = "Informe um CPF válido.")
+	@NotEmpty(message = "CPF é um campo obrigatório.")
 	private String cpfCpnj;
 	
 	private String pessoaTipo;
